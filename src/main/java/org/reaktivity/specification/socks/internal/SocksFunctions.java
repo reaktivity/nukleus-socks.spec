8eb7253cf2fe137ac69e877c5c03ea12d11825e1
jin liu <stuc.cs.jin@berkeley.edu>
2019-09-05 16:25:15 -0700

    update the literal .

--- src/main/java/org/reaktivity/specification/socks/internal/SocksFunctions.java
+++ src/main/java/org/reaktivity/specification/socks/internal/SocksFunctions.java
@@ -19,6 +19,7 @@ import org.agrona.MutableDirectBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
 import org.kaazing.k3po.lang.el.Function;
 import org.kaazing.k3po.lang.el.spi.FunctionMapperSpi;
+import org.reaktivity.specification.socks.internal.types.SocksAddressFW;
 import org.reaktivity.specification.socks.internal.types.control.SocksRouteExFW;
 import org.reaktivity.specification.socks.internal.types.stream.SocksBeginExFW;
 
@@ -32,9 +33,9 @@ public final class SocksFunctions
     private static final int MAX_BUFFER_SIZE = 1024 * 8;
     private static final Pattern IPV4_ADDRESS_PATTERN =
         Pattern.compile("(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" +
-                        ".(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" +
-                        ".(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" +
-                        ".(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+                        "\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" +
+                        "\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" +
+                        "\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
     private static final ThreadLocal<Matcher> IPV4_ADDRESS_MATCHER =
         ThreadLocal.withInitial(() -> IPV4_ADDRESS_PATTERN.matcher(""));
     private static final Pattern IPV6_ADDRESS_PATTERN =
