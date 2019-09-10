72a8b6c7aff6fc68db56c7330720288028c16d53
jin liu <stuc.cs.jin@berkeley.edu>
2019-09-10 10:03:40 -0700

    add one leading 0 tests

--- src/test/java/org/reaktivity/specification/socks/internal/SocksFunctionsTest.java
+++ src/test/java/org/reaktivity/specification/socks/internal/SocksFunctionsTest.java
@@ -158,7 +158,7 @@ public class SocksFunctionsTest
     public void shouldBuildRouteExWithIpv6AddressLeadingZeros() throws Exception
     {
         byte[] bytes = SocksFunctions.routeEx()
-            .address("2001:0db8:85a3:0000:0000:8a2e:0370:7334")
+            .address("2001:0db8:85a3:0:0:8a2e:0370:7334")
             .port(8080)
             .build();
         DirectBuffer buffer = new UnsafeBuffer(bytes);
